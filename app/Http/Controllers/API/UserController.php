5e24a0004157e8e5969a338fa50a90405bfe8f8f
aliaharian <aliaharian@yahoo.com>
2019-11-13 11:35:59 +0330

    auth corr

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -86,6 +86,7 @@ class UserController extends Controller
 
                 oauth_access_token::where('user_id',$user->id)->delete();
                 $success['user']=$user;
+                $success['password']=true;
                 return response()->json(['success'=>$success], $this-> successStatus);
 
             }else{
@@ -97,22 +98,25 @@ class UserController extends Controller
         }
 
 
+        if($user_count==0||$user_count!=0&&$user->password==null) {
 
-        //sms
-        try {
-            $sender = "100065995";
+            //sms
+            try {
+                $sender = "100065995";
 //            09147845149
-            $receptor=[$request->mobile];
-            $message =$code."\n" ."شاورنو" ;
-            $api = new KavenegarApi("614B7A514F4D3067754C4668474E626358616C50356C47467343782B516C6A56");
-            $api->Send($sender, $receptor, $message);
+                $receptor = [$request->mobile];
+                $message = $code . "\n" . "شاورنو";
+                $api = new KavenegarApi("614B7A514F4D3067754C4668474E626358616C50356C47467343782B516C6A56");
+                $api->Send($sender, $receptor, $message);
 
-        } catch (ApiException $e) {
-            // در صورتی که خروجی وب سرویس 200 نباشد این خطا رخ می دهد
-            echo $e->errorMessage();
-        } catch (HttpException $e) {
-            // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
-            echo $e->errorMessage();
+            } catch (ApiException $e) {
+                // در صورتی که خروجی وب سرویس 200 نباشد این خطا رخ می دهد
+                echo $e->errorMessage();
+            } catch (HttpException $e) {
+                // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
+                echo $e->errorMessage();
+            }
+
         }
 
 
