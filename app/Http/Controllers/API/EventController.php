04494432d62e55c29fbeb8a2240e57047fb331a9
aliaharian <aliaharian@yahoo.com>
2019-11-15 17:47:00 +0330

    edit events

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Adviser;
 use App\Event;
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -13,8 +15,16 @@ class EventController extends Controller
     public function show_events()
     {
         $events=Event::orderBy('id','DESC')->paginate(10);
+        $eventArray=array();
+        foreach ($events as $event){
+            $ev=$event;
+            $user=Adviser::find($event->adviser_id);
+            $ev['user_info']=User::select('id','name','avatar')->find($user->user_id);
+            array_push($eventArray,$ev);
+        }
 
-        return response()->json(['success' => $events], $this->successStatus);
+
+        return response()->json(['success' => $eventArray], $this->successStatus);
     }
 
 }
