f19fa81ece7f2cec2e7223960af439efdefd5f86
aliaharian <aliaharian@yahoo.com>
2019-11-29 17:55:02 +0330

    add blog

--- app/Post.php
+++ app/Post.php
@@ -8,17 +8,7 @@ class Post extends Model
 {
     public function categories()
     {
-        $categories=array();
-        $add_cat=$this->belongsToMany(Post_category::class,'post_to_categories');
-        foreach ($add_cat as $cat){
-            $c['id']=$cat->id;
-            $c['title']=$cat->title;
-            $c['image']=$cat->image;
-            $c['parent_category_id']=$cat->parent_category_id;
+        return $this->belongsToMany(Post_category::class,'post_to_categories');
 
-            array_push($categories,$c);
-
-            return $categories;
         }
-    }
 }
