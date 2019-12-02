5456ae20c447097e5cf8519d3aa9728293bbb328
aliaharian <aliaharian@yahoo.com>
2019-12-02 12:01:46 +0330

    edit pay

--- app/Http/Controllers/API/WalletController.php
+++ app/Http/Controllers/API/WalletController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Transaction;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use SaeedVaziry\Payir\Exceptions\SendException;
