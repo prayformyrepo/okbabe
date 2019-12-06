eec415cce07fb717b05fa43fd1d05bce4fa9be0b
aliaharian <aliaharian@yahoo.com>
2019-12-06 16:53:35 +0330

    edit pay

--- resources/views/callback.blade.php
+++ resources/views/callback.blade.php
@@ -104,7 +104,7 @@
 
 <div class="row">
     <div class="col-sm-3"></div>
-    <div class="col-sm-6 subtitle"><a class="btn btn-success btn-lg" href="#">بازگشت به شاورنو</a></div>
+    <div class="col-sm-6 subtitle"><a class="btn btn-success btn-lg" href="com.shavernoapp.shaverno">بازگشت به شاورنو</a></div>
     <div class="col-sm-3"></div>
 </div>
 </div>
