f3fd3627e279e9afb78a1a27aaccfc86856dd10d
aliaharian <aliaharian@yahoo.com>
2019-12-06 16:47:01 +0330

    edit pay

--- app/Http/Controllers/API/WalletController.php
+++ app/Http/Controllers/API/WalletController.php
@@ -16,33 +16,33 @@ use Validator;
 class WalletController extends Controller
 {
     public $successStatus = 200;
+    public $api = 'f1749f4ad693f87cd8c0d95a2b2d6184';
 
     public function do_pay(Request $request)
     {
-        $user=Auth::user();
+        $user = Auth::user();
         $validator = Validator::make($request->all(), [
             'value' => 'required|integer',
         ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
         $amount = $request->value; // Required, Amount
 //        $payir->factorNumber = 'Factor-Number'; // Optional
-        $api='f1749f4ad693f87cd8c0d95a2b2d6184';
-        $redirect='shavernoapp.ir/pay/callback';
-        $description = 'شارژ کیف پول به مبلغ '.
-            $request->value.
+        $redirect = 'shavernoapp.ir/pay/callback';
+        $description = 'شارژ کیف پول به مبلغ ' .
+            $request->value .
             ' تومان '; // Optional
-        $mobile = '0'.$user->mobile; // Optional, If you want to show user's saved card numbers in gateway
+        $mobile = '0' . $user->mobile; // Optional, If you want to show user's saved card numbers in gateway
         $client = new Client([
             // Base URI is used with relative requests
             'base_uri' => 'https://pay.ir',
             // You can set any number of default request options.
-            'timeout'  => 2.0,
+            'timeout' => 2.0,
         ]);
         $response = $client->request('POST', '/pg/send', [
             'form_params' => [
-                'api' => $api,
+                'api' => $this->api,
                 'amount' => $amount,
                 'description' => $description,
                 'mobile' => $mobile,
@@ -53,18 +53,17 @@ class WalletController extends Controller
 
         $body = $response->getBody();
         $items = json_decode($body);
-        $token=$items->token;
+        $token = $items->token;
 
-        $wallet=new Wallet();
-        $wallet->user_id=Auth::user()->id;
-        $wallet->finance=$amount;
-        $wallet->payment_method_id=$token;
+        $wallet = new Wallet();
+        $wallet->user_id = Auth::user()->id;
+        $wallet->finance = $amount;
+        $wallet->payment_method_id = $token;
         $wallet->save();
-        $success['url']='https://pay.ir/pg/'.$token;
+        $success['url'] = 'https://pay.ir/pg/' . $token;
 
 // Implicitly cast the body to a string and echo it
-        return response()->json(['success' => $success], $this-> successStatus);
-
+        return response()->json(['success' => $success], $this->successStatus);
 
 
     }
@@ -72,19 +71,38 @@ class WalletController extends Controller
 
     public function verify(Request $request)
     {
-           $token=$request->token;
-           $status=$request->status;
-           $user_id=Wallet::where('payment_method_id',$token)->value('user_id');
-           $user=User::find($user_id);
-           $amount=Wallet::where('payment_method_id',$token)->value('finance');
+        $token = $request->token;
+        $status = $request->status;
+        $user_id = Wallet::where('payment_method_id', $token)->value('user_id');
+        $wallet_id = Wallet::where('payment_method_id', $token)->value('id');
+        $user = User::find($user_id);
+        $amount = Wallet::where('payment_method_id', $token)->value('finance');
 
-           if ($status==1){
+        if ($status == 1) {
+            $client = new Client([
+                // Base URI is used with relative requests
+                'base_uri' => 'https://pay.ir',
+                // You can set any number of default request options.
+                'timeout' => 2.0,
+            ]);
+            $response = $client->post( '/pg/verify', [
+                'form_params' => [
+                    'api' => $this->api,
+                    'token' => $request->token
+                ]
+            ]);
+            $body = $response->getBody();
+            $body=json_decode($body);
+           // echo $body;
+            $wallet=Wallet::find($wallet_id);
+            $wallet->status=1;
+            $wallet->trans_id=$body->transId;
+            $wallet->save();
 
-           }
-           return view('callback',compact('user','amount','status'));
+        }
+        return view('callback', compact('user', 'amount', 'status'));
 
     }
-
 
 
 }
