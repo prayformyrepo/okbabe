4244dedd23271787ce35c8ca965e167292cf1362
aliaharian <aliaharian@yahoo.com>
2019-12-07 11:09:51 +0330

    edit questions

--- app/Http/Controllers/API/QuestionController.php
+++ app/Http/Controllers/API/QuestionController.php
@@ -86,6 +86,7 @@ class QuestionController extends Controller
             foreach ($questions as $question) {
                 $save['question'] = $question;
                 $save['question']['user_info'] = User::select('id', 'username', 'avatar')->find($question->user_id);
+                $save['question']['user_info']['now'] = Carbon::now()->format('Y-m-d H:i:s');
                 array_push($questions_array, $save);
             }
 
@@ -114,7 +115,7 @@ class QuestionController extends Controller
                 foreach ($questions as $question) {
                     $save['question'] = $question;
                     $save['question']['user_info'] = User::select('id', 'username', 'avatar')->find($question->user_id);
-                    $save['question']['now'] = Carbon::now()->format('Y-m-d H:i:s');
+                    $save['question']['user_info']['now'] = Carbon::now()->format('Y-m-d H:i:s');
                     array_push($questions_array, $save);
                 }
             }
