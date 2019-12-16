abb3620f7a7009655869499bdce773d5f64ea750
aliaharian <aliaharian@yahoo.com>
2019-12-16 14:33:52 +0330

    test

--- app/Http/Controllers/API/AdviserController.php
+++ app/Http/Controllers/API/AdviserController.php
@@ -26,58 +26,68 @@ class AdviserController extends Controller
 
     public function show_advisers(Request $request)
     {
-        if (isset($request->show_online) && $request->show_online=true) {
-            $advisers = Adviser::where('is_online',1)->orderBy('id','DESC')->paginate(10);
-        }
-        else if (isset($request->user_id)){
-            $a=array();
-            if(User::find($request->user_id)->is_adviser==0) return response()->json(['error' => 'user is not adviser'], $this->successStatus);
-            $advisers_id = Adviser::where('user_id',$request->user_id)->value('id');
-            $advisers=Adviser::find($advisers_id);
-            $adviser['adviser']=$advisers;
-            $adviser['adviser']['RN_field']=str_replace('<br>',' - ', str_replace("\n",'',$advisers->field));
-            $adviser['adviser']['RN_about']=str_replace('<br>','',$advisers->about);
-            $adviser['adviser']['name']=User::find($request->user_id)->name;
-            $adviser['adviser']['avatar']=User::find($request->user_id)->avatar;
+        if (isset($request->show_online) && $request->show_online = true) {
+            $advisers = Adviser::where('is_online', 1)->orderBy('id', 'DESC')->paginate(10);
+        } else if (isset($request->user_id)) {
+            $a = array();
+            if (User::find($request->user_id)->is_adviser == 0) return response()->json(['error' => 'user is not adviser'], $this->successStatus);
+            $advisers_id = Adviser::where('user_id', $request->user_id)->value('id');
+            $advisers = Adviser::find($advisers_id);
+            $adviser['adviser'] = $advisers;
+            $adviser['adviser']['RN_field'] = str_replace('<br>', ' - ', str_replace("\n", '', $advisers->field));
+            $adviser['adviser']['RN_about'] = str_replace('<br>', '', $advisers->about);
+            $adviser['adviser']['name'] = User::find($request->user_id)->name;
+            $adviser['adviser']['avatar'] = User::find($request->user_id)->avatar;
 //            $adviser['adviser']['categories']=$advisers->categories()->get();
-            $adviser['adviser']['times']=$advisers->times()->get();
-            $adviser['adviser']['qa_count']=Question_answer::where('adviser_id',$advisers->id)->count();
-            $adviser['adviser']['comment_count']=Adviser_rate::where('adviser_id',$advisers->id)->where('comment','!=',null)->count();
-            $adviser['adviser']['call_count']=Call::where('adviser_id',$advisers->id)->where('duration','!=',null)->count();
-            $save_count=Saved_adviser::where('user_id',Auth::user()->id)->where('adviser_id',$advisers_id)->count();
-            $adviser['adviser']['is_saved']=$save_count==0?0:1;
+//            $adviser['adviser']['times']=$advisers->times()->get();
+            $times = $advisers->times()->orderBy('date', 'ASC')->get();
+            $tt = $times;
+            foreach ($times as $time) {
+                foreach ($times as $t) {
+                    if ($time->date == $t->date) {
+                        $tt['date'] = null;
+                    }
+                }
+            }
+            dd($tt);
 
-            array_push($a,$adviser);
+
+            $adviser['adviser']['qa_count'] = Question_answer::where('adviser_id', $advisers->id)->count();
+            $adviser['adviser']['comment_count'] = Adviser_rate::where('adviser_id', $advisers->id)->where('comment', '!=', null)->count();
+            $adviser['adviser']['call_count'] = Call::where('adviser_id', $advisers->id)->where('duration', '!=', null)->count();
+            $save_count = Saved_adviser::where('user_id', Auth::user()->id)->where('adviser_id', $advisers_id)->count();
+            $adviser['adviser']['is_saved'] = $save_count == 0 ? 0 : 1;
+
+            array_push($a, $adviser);
             return response()->json(['success' => $a], $this->successStatus);
+        } else {
+            $advisers = Adviser::orderBy('id', 'DESC')->paginate(10);
         }
-        else{
-            $advisers = Adviser::orderBy('id','DESC')->paginate(10);
-        }
-        $a=array();
-        foreach ($advisers as $advise){
-           $adviser['adviser']=$advise;
-            $adviser['adviser']['RN_field']=str_replace('<br>','',$advise->field);
-            $adviser['adviser']['RN_about']=str_replace('<br>','',$advise->about);
-            $adviser['adviser']['name']=User::find($advise->user_id)->name;
-            $adviser['adviser']['avatar']=User::find($advise->user_id)->avatar;
-            $adviser['adviser']['categories']=$advise->categories()->get();
-            $adviser['adviser']['times']=$advise->times()->get();
-            $adviser['adviser']['qa_count']=Question_answer::where('adviser_id',$advise->id)->count();
-            $adviser['adviser']['comment_count']=Adviser_rate::where('adviser_id',$advise->id)->where('comment','!=',null)->count();
-            $adviser['adviser']['call_count']=Call::where('adviser_id',$advise->id)->where('duration','!=',null)->count();
-            $save_count=Saved_adviser::where('user_id',Auth::user()->id)->where('adviser_id',$advise->id)->count();
-            $adviser['adviser']['is_saved']=$save_count==0?0:1;
+        $a = array();
+        foreach ($advisers as $advise) {
+            $adviser['adviser'] = $advise;
+            $adviser['adviser']['RN_field'] = str_replace('<br>', '', $advise->field);
+            $adviser['adviser']['RN_about'] = str_replace('<br>', '', $advise->about);
+            $adviser['adviser']['name'] = User::find($advise->user_id)->name;
+            $adviser['adviser']['avatar'] = User::find($advise->user_id)->avatar;
+            $adviser['adviser']['categories'] = $advise->categories()->get();
+            $adviser['adviser']['times'] = $advise->times()->get();
+            $adviser['adviser']['qa_count'] = Question_answer::where('adviser_id', $advise->id)->count();
+            $adviser['adviser']['comment_count'] = Adviser_rate::where('adviser_id', $advise->id)->where('comment', '!=', null)->count();
+            $adviser['adviser']['call_count'] = Call::where('adviser_id', $advise->id)->where('duration', '!=', null)->count();
+            $save_count = Saved_adviser::where('user_id', Auth::user()->id)->where('adviser_id', $advise->id)->count();
+            $adviser['adviser']['is_saved'] = $save_count == 0 ? 0 : 1;
 
-            array_push($a,$adviser);
+            array_push($a, $adviser);
         }
-        $paginate['total']=$advisers->total();
-        $paginate['per_page']=$advisers->perPage();
-        $paginate['current_page']=$advisers->currentPage();
-        $paginate['last_page']=$advisers->lastPage();
-        $paginate['has_more_pages']=$advisers->hasMorePages();
-        $paginate['next_page_url']=$advisers->nextPageUrl();
-        $paginate['previous_page_url']=$advisers->previousPageUrl();
-        return response()->json(['success' => $a,"pagination"=>$paginate], $this->successStatus);
+        $paginate['total'] = $advisers->total();
+        $paginate['per_page'] = $advisers->perPage();
+        $paginate['current_page'] = $advisers->currentPage();
+        $paginate['last_page'] = $advisers->lastPage();
+        $paginate['has_more_pages'] = $advisers->hasMorePages();
+        $paginate['next_page_url'] = $advisers->nextPageUrl();
+        $paginate['previous_page_url'] = $advisers->previousPageUrl();
+        return response()->json(['success' => $a, "pagination" => $paginate], $this->successStatus);
 
     }
 
@@ -95,33 +105,31 @@ class AdviserController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
         $agent = new Agent();
 
-        $user=new User();
-        $user->name=$request->name;
-        $user->mobile=$request->mobile;
-        $user->gender=$request->gender;
-        $user->is_adviser=1;
+        $user = new User();
+        $user->name = $request->name;
+        $user->mobile = $request->mobile;
+        $user->gender = $request->gender;
+        $user->is_adviser = 1;
         $user->os = $agent->platform();
         $user->os_version = $agent->version($agent->platform());
         $user->save();
 
-        $adviser=new Adviser();
-        $adviser->user_id=$user->id;
-        $adviser->field=$request->field;
-        $adviser->about=$request->about;
-        $adviser->call_price=$request->call_price;
-        $adviser->nominal_call_price=$request->nominal_call_price;
+        $adviser = new Adviser();
+        $adviser->user_id = $user->id;
+        $adviser->field = $request->field;
+        $adviser->about = $request->about;
+        $adviser->call_price = $request->call_price;
+        $adviser->nominal_call_price = $request->nominal_call_price;
         $adviser->save();
 
 
+        $adviser = Adviser::find($adviser->id);
 
-
-        $adviser=Adviser::find($adviser->id);
-
-        return response()->json(['success'=>$adviser], $this-> successStatus);
+        return response()->json(['success' => $adviser], $this->successStatus);
 
     }
 
@@ -135,17 +143,17 @@ class AdviserController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
-        $category=new Adviser_category();
-        $category->name=$request->name;
-        $category->slug=$request->slug;
-        isset($request->parent_category_id)?$category->parent_category_id=$request->parent_category_id:$category->save();
+        $category = new Adviser_category();
+        $category->name = $request->name;
+        $category->slug = $request->slug;
+        isset($request->parent_category_id) ? $category->parent_category_id = $request->parent_category_id : $category->save();
         $category->save();
 
-        $category=Adviser_category::find($category->id);
+        $category = Adviser_category::find($category->id);
 
-        return response()->json(['success'=>$category], $this-> successStatus);
+        return response()->json(['success' => $category], $this->successStatus);
     }
 
     public function assign_adviser_to_category(Request $request)
@@ -156,10 +164,10 @@ class AdviserController extends Controller
 
         ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
-        $assign=Adviser_to_category::where('adviser_id',$request->adviser_id)->where('adviser_category_id',$request->adviser_category_id)->count();
-        if ($assign==0) {
+        $assign = Adviser_to_category::where('adviser_id', $request->adviser_id)->where('adviser_category_id', $request->adviser_category_id)->count();
+        if ($assign == 0) {
 
             $assign = new Adviser_to_category();
             $assign->adviser_id = $request->adviser_id;
@@ -167,8 +175,8 @@ class AdviserController extends Controller
             $assign->save();
 
         }
-        $adviser=Adviser::find($request->adviser_id)->categories()->get();
-        return response()->json(['success'=>$adviser], $this-> successStatus);
+        $adviser = Adviser::find($request->adviser_id)->categories()->get();
+        return response()->json(['success' => $adviser], $this->successStatus);
 
     }
 
@@ -179,15 +187,15 @@ class AdviserController extends Controller
             'date' => 'required|integer',
             'time_from' => 'required',
             'time_to' => 'required',
-            'adviser_id'=>'required'
+            'adviser_id' => 'required'
 
         ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
-        $time=Adviser_time::where('adviser_id',$request->adviser_id)->where('date',$request->date)->where('time_from',$request->time_from)->where('time_to',$request->time_to)->count();
+        $time = Adviser_time::where('adviser_id', $request->adviser_id)->where('date', $request->date)->where('time_from', $request->time_from)->where('time_to', $request->time_to)->count();
 
-        if ($time==0) {
+        if ($time == 0) {
             $time = new Adviser_time();
             $time->adviser_id = $request->adviser_id;
             $time->date = $request->date;
@@ -195,40 +203,40 @@ class AdviserController extends Controller
             $time->time_to = $request->time_to;
             $time->save();
         }
-        $adviser=Adviser::find($request->adviser_id)->times()->get();
-        return response()->json(['success'=>$adviser], $this-> successStatus);
+        $adviser = Adviser::find($request->adviser_id)->times()->get();
+        return response()->json(['success' => $adviser], $this->successStatus);
     }
 
     public function save_adviser(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'adviser_id'=>'required'
+            'adviser_id' => 'required'
 
         ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
 
-        $user=Auth::user();
+        $user = Auth::user();
 
-        $adviser=Adviser::find($request->adviser_id);
-        if ($adviser==null){
-            $message='adviser not found';
-            return response()->json(['error'=>$message], 404);
+        $adviser = Adviser::find($request->adviser_id);
+        if ($adviser == null) {
+            $message = 'adviser not found';
+            return response()->json(['error' => $message], 404);
         }
 
-        $saved_adviser=Saved_adviser::where('user_id',$user->id)->where('adviser_id',$request->adviser_id);
-        if ($saved_adviser->count()==0) {
+        $saved_adviser = Saved_adviser::where('user_id', $user->id)->where('adviser_id', $request->adviser_id);
+        if ($saved_adviser->count() == 0) {
             $save = new Saved_adviser();
             $save->user_id = $user->id;
             $save->adviser_id = $request->adviser_id;
             $save->save();
-            $success['message']='created';
+            $success['message'] = 'created';
 
 
             //add to events
-            $event=Event::where('user_id',$user->id)->where('adviser_id',$request->adviser_id);
-            if($event->count()==0) {
+            $event = Event::where('user_id', $user->id)->where('adviser_id', $request->adviser_id);
+            if ($event->count() == 0) {
                 $event = new Event();
                 $event->type = 0;
                 $event->adviser_id = $request->adviser_id;
@@ -236,21 +244,21 @@ class AdviserController extends Controller
                 $event->save();
             }
 
-        }else{
-            $saved_adviser_id=$saved_adviser->value('id');
+        } else {
+            $saved_adviser_id = $saved_adviser->value('id');
             Saved_adviser::find($saved_adviser_id)->delete();
-            $success['message']='deleted';
+            $success['message'] = 'deleted';
 
         }
 
-        return response()->json(['success'=>$success], $this-> successStatus);
+        return response()->json(['success' => $success], $this->successStatus);
 
     }
 
     public function show_adviser_categories()
     {
-        $adviser_category=Adviser_category::orderBy('id','DESC')->simplePaginate(10);
-        return response()->json(['success'=>$adviser_category], $this-> successStatus);
+        $adviser_category = Adviser_category::orderBy('id', 'DESC')->simplePaginate(10);
+        return response()->json(['success' => $adviser_category], $this->successStatus);
     }
 
 
@@ -258,27 +266,27 @@ class AdviserController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'adviser_id' => 'required',
-            'rate'=>'required',
-            'is_private'=>'required'
+            'rate' => 'required',
+            'is_private' => 'required'
 //
         ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
-        $adviser_r=@Adviser_rate::where('user_id',Auth::user()->id)->where('adviser_id',$request->adviser_id)->get();
-        $adviser_r=$adviser_r->count();
-        if ($adviser_r==0) {
+        $adviser_r = @Adviser_rate::where('user_id', Auth::user()->id)->where('adviser_id', $request->adviser_id)->get();
+        $adviser_r = $adviser_r->count();
+        if ($adviser_r == 0) {
             $rate = new Adviser_rate();
             $rate->adviser_id = $request->adviser_id;
             $rate->user_id = Auth::user()->id;
             $rate->rate = $request->rate;
-            $rate->is_private=$request->is_private;
+            $rate->is_private = $request->is_private;
             if (isset($request->comment)) $rate->comment = $request->comment;
             $rate->save();
-            return response()->json(['success'=>$rate], $this-> successStatus);
+            return response()->json(['success' => $rate], $this->successStatus);
 
         }
-        return response()->json(['success'=>'ok'], $this-> successStatus);
+        return response()->json(['success' => 'ok'], $this->successStatus);
 
     }
 
@@ -286,17 +294,17 @@ class AdviserController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'accept' => 'required',
-            'rate_id'=>'required'
+            'rate_id' => 'required'
 
 //
         ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
-        $rate=Adviser_rate::find($request->rate_id);
-        $rate->status=$request->accept;
+        $rate = Adviser_rate::find($request->rate_id);
+        $rate->status = $request->accept;
         $rate->save();
-        return response()->json(['success'=>$rate], $this-> successStatus);
+        return response()->json(['success' => $rate], $this->successStatus);
     }
 
     public function reach_adviser(Request $request)
@@ -307,13 +315,13 @@ class AdviserController extends Controller
 //
         ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
 
-        $adviser=Adviser::find($request->adviser_id);
-        $adviser->status=1;
+        $adviser = Adviser::find($request->adviser_id);
+        $adviser->status = 1;
         $adviser->save();
-        return response()->json(['success'=>$adviser], $this-> successStatus);
+        return response()->json(['success' => $adviser], $this->successStatus);
     }
 
 }
