89f4af81ab136e4ab5bdda5057c996fde8033e0e
aliaharian <aliaharian@yahoo.com>
2019-12-19 19:32:39 +0330

    edit welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,7 +74,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    شاورنو در حال ساخت است!
+                   COMING SOON ...
                 </div>
 
             </div>
