bfe9a84243acc768774baf40ed61d54c991f5c91
aliaharian <aliaharian@yahoo.com>
2019-12-23 19:07:59 +0330

    add filter to events

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    public function categories()
+    {
+        return Adviser::find($this->adviser_id)->categories;
+    }
 }
