528fbbf2a6511800fdfe925a0f80f04990854e54
aliaharian <aliaharian@yahoo.com>
2019-12-23 17:48:46 +0330

    edit show post

--- app/Http/Controllers/API/BlogController.php
+++ app/Http/Controllers/API/BlogController.php
@@ -20,7 +20,67 @@ class BlogController extends Controller
         if (isset($request->post_id)){
             $posts=Post::find($request->post_id);
             $posts['categories']=$posts->categories()->get();
-        }else{
+        }
+        else if (isset($request->category)){
+            if ($request->category=='0') { //nino
+                $category_id=149518063;
+                $show_posts = Post::select('id', 'title', 'description', 'image', 'created_at')->orderBy('id', 'DESC')->paginate(10);
+                $posts = array();
+                foreach ($show_posts as $post) {
+                    $categories=$post->categories()->get();
+                    $flag=false;
+
+                    foreach ($categories as $category){
+                        if ($category->id==$category_id) $flag=true;
+                    }
+                    if ($flag==true) {
+                        $p = $post;
+                        $p['categories'] = $post->categories()->get();
+                        array_push($posts, $p);
+                    }
+                }
+            }
+
+            if ($request->category=='1') { //shino
+                $category_id=149048146;
+                $show_posts = Post::select('id', 'title', 'description', 'image', 'created_at')->orderBy('id', 'DESC')->paginate(10);
+                $posts = array();
+                foreach ($show_posts as $post) {
+                    $categories=$post->categories()->get();
+                    $flag=false;
+
+                    foreach ($categories as $category){
+                        if ($category->id==$category_id) $flag=true;
+                    }
+                    if ($flag==true) {
+                        $p = $post;
+                        $p['categories'] = $post->categories()->get();
+                        array_push($posts, $p);
+                    }
+                }
+            }
+
+            if ($request->category=='2') { //dino
+                $category_id=149519032;
+                $show_posts = Post::select('id', 'title', 'description', 'image', 'created_at')->orderBy('id', 'DESC')->paginate(10);
+                $posts = array();
+                foreach ($show_posts as $post) {
+                    $categories=$post->categories()->get();
+                    $flag=false;
+
+                    foreach ($categories as $category){
+                        if ($category->id==$category_id) $flag=true;
+                    }
+                    if ($flag==true) {
+                        $p = $post;
+                        $p['categories'] = $post->categories()->get();
+                        array_push($posts, $p);
+                    }
+                }
+            }
+        }
+
+        else{
             $show_posts=Post::select('id','title','description','image','created_at')->orderBy('id','DESC')->paginate(10);
             $posts=array();
             foreach ($show_posts as $post){
