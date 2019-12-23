a886cb7366817680ba9bfeb9c39748e4a08b7cce
aliaharian <aliaharian@yahoo.com>
2019-12-23 18:43:09 +0330

    add filter to events

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Adviser;
 use App\Adviser_category;
+use App\Adviser_to_category;
 use App\Event;
 use App\Http\Controllers\Controller;
 use App\User;
@@ -16,34 +17,69 @@ class EventController extends Controller
 
     public function show_events(Request $request)
     {
-        if (isset($request->type)) $events=Event::where('type',$request->type)->orderBy('id','DESC')->paginate(10);
-        else $events=Event::orderBy('id','DESC')->paginate(10);
-        $eventArray=array();
-        foreach ($events as $event){
-            $ev=$event;
-            $user=Adviser::find($event->adviser_id);
-            if($event->type==1){
-                if ($event->category_id!=null)
-                    $ev['category_name']=Adviser_category::find($event->category_id)->name;
+        if (isset($request->type)) {
+            if (isset($request->category_id)) {
+                $evs = Event::where('type', $request->type)->orderBy('id', 'DESC')->paginate(10);
+                $events = array();
+                foreach ($evs as $ev) {
+                    $adviser_categories = Adviser_to_category::where('adviser_id', $ev->adviser_id)->get();
+                    $flag = false;
+                    foreach ($adviser_categories as $adviser_category) {
+                        if ($adviser_category->adviser_category_id == $request->category_id) $flag = true;
+                    }
+                    if ($flag == true) {
+                        array_push($events,$ev);
+                    }
+                }
+            } else {
+                $events = Event::where('type', $request->type)->orderBy('id', 'DESC')->paginate(10);
             }
-            $ev['now']=Carbon::now()->format('Y-m-d H:i:s');
+        } else {
+            if (isset($request->category_id)) {
+                $evs = Event::orderBy('id', 'DESC')->paginate(10);
+                $events = array();
+                foreach ($evs as $ev) {
+                    $adviser_categories = Adviser_to_category::where('adviser_id', $ev->adviser_id)->get();
+                    $flag = false;
+                    foreach ($adviser_categories as $adviser_category) {
+                        if ($adviser_category->adviser_category_id == $request->category_id) $flag = true;
+                    }
+                    if ($flag == true) {
+                        array_push($events,$ev);
+                    }
+                }
 
-            $ev['user_info']=User::select('id','name','avatar')->find($user->user_id);
-            $ev['user_info']['about']=Adviser::find($event->adviser_id)->about;
-            $ev['user_info']['field']=Adviser::find($event->adviser_id)->field;
-            $ev['user_info']['is_online']=Adviser::find($event->adviser_id)->is_online;
-            array_push($eventArray,$ev);
+            }else {
+                $events = Event::orderBy('id', 'DESC')->paginate(10);
+            }
         }
 
-        $paginate['total']=$events->total();
-        $paginate['per_page']=$events->perPage();
-        $paginate['current_page']=$events->currentPage();
-        $paginate['last_page']=$events->lastPage();
-        $paginate['has_more_pages']=$events->hasMorePages();
-        $paginate['next_page_url']=$events->nextPageUrl();
-        $paginate['previous_page_url']=$events->previousPageUrl();
+        $eventArray = array();
+        foreach ($events as $event) {
+            $ev = $event;
+            $user = Adviser::find($event->adviser_id);
+            if ($event->type == 1) {
+                if ($event->category_id != null)
+                    $ev['category_name'] = Adviser_category::find($event->category_id)->name;
+            }
+            $ev['now'] = Carbon::now()->format('Y-m-d H:i:s');
 
-        return response()->json(['success' => $eventArray,'pagination'=>$paginate], $this->successStatus);
+            $ev['user_info'] = User::select('id', 'name', 'avatar')->find($user->user_id);
+            $ev['user_info']['about'] = Adviser::find($event->adviser_id)->about;
+            $ev['user_info']['field'] = Adviser::find($event->adviser_id)->field;
+            $ev['user_info']['is_online'] = Adviser::find($event->adviser_id)->is_online;
+            array_push($eventArray, $ev);
+        }
+
+        $paginate['total'] = $events->total();
+        $paginate['per_page'] = $events->perPage();
+        $paginate['current_page'] = $events->currentPage();
+        $paginate['last_page'] = $events->lastPage();
+        $paginate['has_more_pages'] = $events->hasMorePages();
+        $paginate['next_page_url'] = $events->nextPageUrl();
+        $paginate['previous_page_url'] = $events->previousPageUrl();
+
+        return response()->json(['success' => $eventArray, 'pagination' => $paginate], $this->successStatus);
     }
 
 }
