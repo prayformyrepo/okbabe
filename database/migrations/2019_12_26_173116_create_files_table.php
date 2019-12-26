1c452137bea8a5ae159e2b8012bd547fb48dda30
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-12-26 20:33:17 +0330

    Create 2019_12_26_173116_create_files_table.php

---
+++ database/migrations/2019_12_26_173116_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('type')->default(1); //type 1 is product
+            $table->text('file_path');
+            $table->text('orginal_name');
+            $table->string('file_type')->nullable(); //zip or pdf
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
