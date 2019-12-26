e7186caa9f2177d667bab56a0f9e0b3affeade83
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-12-26 20:32:38 +0330

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $hidden = ['pivot'];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Product_category::class,'product_to_categories')->select('id','name');
+    }
+
+    public function productType()
+    {
+        return $this->belongsTo(Product_type::class);
+    }
+
+    public function images()
+    {
+        return $this->belongsToMany(Image::class,'product_images')->select('id','image_path','thumbnail_path');
+    }
 }
