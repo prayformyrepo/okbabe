3c579095560df5fe7b50191d82fa14ab84fdf0c7
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-12-27 18:27:29 +0330

    Update Product_comment.php

--- app/Product_comment.php
+++ app/Product_comment.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product_comment extends Model
 {
+
     protected $fillable = ['title','text','status','product_id','user_id'];
+    protected $table = 'product_comments';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
