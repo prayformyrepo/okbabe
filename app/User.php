1e613c26859c3f2f26ecbb06a14c2fcc03e6353d
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-12-27 18:27:36 +0330

    Update User.php

--- app/User.php
+++ app/User.php
@@ -50,5 +50,14 @@ class User extends Authenticatable
         return $this->hasMany(Ticket::class);
     }
 
+    public function toArray()
+    {
+        return [
+          'id'=>$this->id ,
+           'name'=> $this->name != null ? $this->name : $this->username  ,
+           'avatar'=>$this->avatar
+        ];
+    }
+
 
 }
