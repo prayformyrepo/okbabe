9af1f64aee88ef0bcb3455de94c04a6b9890c688
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-12-28 20:46:53 +0330

    Create CartResource.php

---
+++ app/Http/Resources/CartResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CartResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+/*        return parent::toArray($request);*/
+        return [
+        'id'=>$this->id,
+        'product'=>new ProductResource($this->product),
+         'price'=>$this->total_price
+
+        ];
+    }
+}
