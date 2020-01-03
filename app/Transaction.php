c0f33acd309392dbb9cc22b25c95a79fb5bae24d
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2020-01-03 21:47:56 +0330

    Update Transaction.php

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['user_id','amount','payment_method_id','authority','refid','state'];
 }
