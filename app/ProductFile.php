5a250ffe30d6c4bef908399d044742266d13d939
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2020-01-03 21:47:46 +0330

    Create ProductFile.php

---
+++ app/ProductFile.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductFile extends Model
+{
+    protected $fillable = ['type','file_path','orginal_name','file_type','slug'];
+}
