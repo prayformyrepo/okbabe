00aeabff5ec45a799b955fb5bcb649a22bac1131
aliaharian <aliaharian@yahoo.com>
2020-01-08 14:11:32 +0330

    create test controller

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\TestResourceCollection;
 use App\Test;
 use App\TestQuestion;
+use App\UserTestAnswer;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TestController extends Controller
 {
@@ -16,14 +19,40 @@ class TestController extends Controller
     {
         if (isset($request->test_id)){
             $test=TestQuestion::where('test_id',$request->test_id)->where('question_number',$request->question_number)->first();
-            return response()->json(['success' => new TestResourceCollection($test)], $this->successStatus);
 
+            return response()->json(['success' => new TestResourceCollection($test)], $this->successStatus);
         }
 
         $tests=Test::all();
 
+        $tst=array();
+        foreach ($tests as $test){
+            $t['id']=$test->id;
+            $t['name']=$test->name;
+            $t['slug']=$test->slug;
+            $t['description']=$test->description;
+            $t['questions_count']=$test->questions_count;
+            $t['answers_per_question']=$test->answers_per_question;
+            $t['status']=$test->status;
+
+            if(UserTestAnswer::where('user_id',Auth::user()->id)->where('test_question_id',$test->id)->count()==0)
+                $t['last_answered_question']=null;
+                else
+            $t['last_answered_question']= UserTestAnswer::where('user_id',Auth::user()->id)->where('test_question_id',$test->id)->orderBy('test_answer_id','ASC')->last();
+
+
+            $t['created_at']=Carbon::make($test->created_at)->format('Y-m-d H:i:s');
+
+            array_push($tst,$t);
+        }
+
 
         return response()->json(['success' => $tests], $this->successStatus);
+
+    }
+
+    public function save_test(Request $request)
+    {
 
     }
 }
