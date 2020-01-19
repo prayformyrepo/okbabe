bf1c5fc7ab960e0313af70aa4cf5e96337c1e015
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2020-01-19 20:02:37 +0330

    Update User.php

--- app/User.php
+++ app/User.php
@@ -54,4 +54,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Cart::class,'user_id');
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class,'user_id');
+
+    }
 }
