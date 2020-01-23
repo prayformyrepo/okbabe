295b43e413a2c7e28a8f5cd1c855b724dcca8520
aliaharian <aliaharian@yahoo.com>
2020-01-23 14:57:11 +0330

    edit test

--- database/migrations/2020_01_23_133450_add_relations_to_posts_table.php
+++ database/migrations/2020_01_23_133450_add_relations_to_posts_table.php
@@ -14,7 +14,7 @@ class AddRelationsToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->foreign('main_category')->references('id')->on('post_categories')->onDelete('set null');
+//            $table->foreign('main_category')->references('id')->on('post_categories')->onDelete('set null');
         });
     }
 
