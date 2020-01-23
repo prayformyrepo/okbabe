561379a0d8ab68a7a103e2c4b110b7a0c7d8c341
aliaharian <aliaharian@yahoo.com>
2020-01-23 14:31:09 +0330

    edit test

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -39,7 +39,7 @@ class TestController extends Controller
 
             }
 
-            return response()->json(['success' => new $t], $this->successStatus);
+            return response()->json(['success' => $t], $this->successStatus);
         }
 
         $tests=Test::all();
