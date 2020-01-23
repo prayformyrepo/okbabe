24fba1382ea186f47fa55485ba5680ef9b6eec27
aliaharian <aliaharian@yahoo.com>
2020-01-23 19:30:10 +0330

    edit call

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -39,26 +39,31 @@ class ProductController extends Controller
 
         }else{
 
-            $product['product']=['id'=>$product->id,
-                'name'=>$product->name,
-                'description'=>$product->description,
-                'short_description'=>$product->short_description,
-                'language'=>$product->language,
-                'pages'=>$product->pages,
-                'size'=>$product->size,
-                'author'=>$product->author,
-                'announcer'=>$product->announcer,
-                'translator'=>$product->translator,
-                'published_date'=>$product->published_date,
-                'publisher'=>$product->publisher,
-                'productType'=>$product->productType->name,
-                'price'=>$product->price,
-                'featured'=>$product->featured,
-                'images'=>$product->images()->get(),
-                'categories'=>$product->categories()->get(),
-                'comments'=>  ProductCommentResource::Collection($product->productComments()->orderBy('created_at','DESC')->get()),
-                'created_at'=>Carbon::make($product->created_at)->format('Y-m-d H:i:s')
-            ];
+//            $product['product']=['id'=>$product->id,
+//                'name'=>$product->name,
+//                'description'=>$product->description,
+//                'short_description'=>$product->short_description,
+//                'language'=>$product->language,
+//                'pages'=>$product->pages,
+//                'size'=>$product->size,
+//                'author'=>$product->author,
+//                'announcer'=>$product->announcer,
+//                'translator'=>$product->translator,
+//                'published_date'=>$product->published_date,
+//                'publisher'=>$product->publisher,
+//                'productType'=>$product->productType->name,
+//                'price'=>$product->price,
+//                'featured'=>$product->featured,
+//                'images'=>$product->images()->get(),
+//                'categories'=>$product->categories()->get(),
+//                'comments'=>  ProductCommentResource::Collection($product->productComments()->orderBy('created_at','DESC')->get()),
+//                'created_at'=>Carbon::make($product->created_at)->format('Y-m-d H:i:s')
+//            ];
+            $product['product']=$product;
+            $product['product']['images']=$product->images()->get();
+            $product['product']['categories']=$product->categories()->get();
+            $product['product']['comments']=ProductCommentResource::Collection($product->productComments()->orderBy('created_at','DESC')->get());
+
         return response()->json(['success'=>$product]);
 
         }
