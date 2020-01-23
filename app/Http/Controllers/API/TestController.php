9edda0ac5439b423d3593108c59a1256e7574d7f
aliaharian <aliaharian@yahoo.com>
2020-01-23 16:05:43 +0330

    edit test

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -145,7 +145,7 @@ class TestController extends Controller
 
         $text='
 <img src="http://shavernoapp.ir/images/shsh.png" width="200px" style="border: 2px red dashed">
-<p>تست شما ثبت شد و پاسخ آن برایتان ارسال می گردد. به زودی قابلیت تصحیح تست به صورت لحظه ای در اپلیکیشن شاورنو اضافه خواهد شد</p>';
+<p style="border-top: 2px green dot-dash">تست شما ثبت شد و پاسخ آن برایتان ارسال می گردد. به زودی قابلیت تصحیح تست به صورت لحظه ای در اپلیکیشن شاورنو اضافه خواهد شد</p>';
         return response()->json(['success' => $text], 200);
 
 
