dea8e2e6095b6fd253bcc34582189e09279c2efb
aliaharian <aliaharian@yahoo.com>
2020-01-23 13:20:16 +0330

    add migrations

--- database/migrations/2020_01_23_115130_add_relations_to_events_table.php
+++ database/migrations/2020_01_23_115130_add_relations_to_events_table.php
@@ -17,7 +17,7 @@ class AddRelationsToEventsTable extends Migration
             $table->unsignedInteger('user_id')->index()->change();
             $table->unsignedBigInteger('adviser_id')->index()->change();
 
-            $table->foreign('adviser_id')->references('id')->on('advisers')->onDelete('cascade');
+//            $table->foreign('adviser_id')->references('id')->on('advisers')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('adviser_categories')->onDelete('cascade');
 
