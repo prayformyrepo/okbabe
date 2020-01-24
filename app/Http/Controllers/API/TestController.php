496a2f0b11ca20ad949a644a5b18dd1be2c5be7c
aliaharian <aliaharian@yahoo.com>
2020-01-24 12:01:45 +0330

    enrich47 algorithm

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -21,6 +21,9 @@ class TestController extends Controller
     public function show_tests(Request $request)
     {
         if (isset($request->test_id) && isset($request->question_number)) {
+            if (isset($request->reset) && $request->reset==1){
+                $user_answers=UserTestAnswer::where('user_id',Auth::user()->id)->delete();
+            }
             $number=Test::find($request->test_id)->questions_count;
             if($request->question_number > $number ) return response()->json(['success' => 'ended'], $this->successStatus);
             $test=TestQuestion::where('test_id',$request->test_id)->where('question_number',$request->question_number)->first();
