c8108ac7ea191c2f43588cc5267a43a7f7a6d4dd
aliaharian <aliaharian@yahoo.com>
2020-01-24 16:59:48 +0330

    add adviser comments

--- app/Http/Controllers/API/ConversationController.php
+++ app/Http/Controllers/API/ConversationController.php
@@ -254,9 +254,9 @@ class ConversationController extends Controller
             $me['text'] = $m->text;
             $me['file_id'] = $m->file_id;
 //            $file_id=$m->file_id;
-            $me['file_name'] = File::find($m->file_id)->orginal_name;
-            $me['file_path'] = File::find($m->file_id)->file_path;
-            $me['file_type'] = File::find($m->file_id)->file_type;
+            $me['file_name'] = File::find($m->file_id);
+//            $me['file_path'] = File::find($m->file_id)->file_path;
+//            $me['file_type'] = File::find($m->file_id)->file_type;
 
             $me['status'] = $m->status;
             $me['user_id'] = $m->user_id;
