55ec8d3b5bf6c9181eb6b48951a4c11627932f95
aliaharian <aliaharian@yahoo.com>
2020-01-26 01:56:07 +0330

    add blog blade

--- resources/views/show_post.blade.php
+++ resources/views/show_post.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$post->title}}</title>
 </head>
-<body>
+<body style="text-align: right;">
 {!! $post->content !!}
 </body>
 </html>
