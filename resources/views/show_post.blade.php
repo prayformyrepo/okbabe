6558a4b7fb1035ac3324a4d161f55129296a30ee
aliaharian <aliaharian@yahoo.com>
2020-01-26 01:57:12 +0330

    add blog blade

--- resources/views/show_post.blade.php
+++ resources/views/show_post.blade.php
@@ -6,6 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$post->title}}</title>
+    <style>
+        p{
+            text-align: right!important;
+        }
+    </style>
 </head>
 <body style="text-align: right;">
 {!! $post->content !!}
