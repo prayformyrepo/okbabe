79f4baa6a174f4858440e4b0097b9847e65c53d2
aliaharian <aliaharian@yahoo.com>
2020-01-26 02:06:25 +0330

    add blog blade

--- resources/views/show_post.blade.php
+++ resources/views/show_post.blade.php
@@ -7,8 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$post->title}}</title>
     <style>
+        @font-face {
+            font-family: 'iranSans';
+            src:url('/iranSans/eot/IRANSansWeb.eot');
+            src:url('/iranSans/eot/IRANSansWeb.eot?#iefix') format('embedded-opentype'),
+            url('/iranSans/woff/IRANSansWeb.woff') format('woff'),
+            url('/iranSans/ttf/IRANSansWeb.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
         *{
             direction: rtl;
+        }
+        p,h1,h2,h3,h4,h5,h6,span,strong,div{
+            font-family: 'iranSans';
         }
     </style>
 </head>
